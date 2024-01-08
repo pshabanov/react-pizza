fade5e91c3db29f28178b0fc13f985aaa7a8bb6e
Pavel Shabanov <pshabanov@nurtelecom.kg>
2024-01-08 16:46:56 +0600

    Добавил eslint + prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaList from "./components/PizzaList";
-import {useState} from "react";
+import { useState } from 'react'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import PizzaList from './components/PizzaList'
 import pizzas from './pizzas'
 
-
 function App() {
-
-    const [pizza, setPizza] = useState(pizzas)
-    return (
-        <div className="wrapper">
-            <Header/>
-            <div className="content">
-                <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <PizzaList items={pizza} />
-                </div>
-            </div>
+  const [pizza, setPizza] = useState(pizzas)
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <PizzaList items={pizza} />
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
